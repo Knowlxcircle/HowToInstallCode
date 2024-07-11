cc775fa6cebcc12f0ffc39fb9e22e6dffaac65e1
Thirza <thirzahmad@gmail.com>
2024-07-11 16:46:33 +0700

    add

--- howtoinstallcode/settings.py
+++ howtoinstallcode/settings.py
@@ -132,3 +132,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "https://howtoinstallcode.domcloud.dev"]
