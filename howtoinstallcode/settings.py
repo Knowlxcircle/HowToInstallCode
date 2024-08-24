f13b08756fc0eb817a9b06e91994a8b8aa9c8bfb
thirzq <thirzahmad@gmail.com>
2024-08-24 21:39:30 +0700

    add static

--- howtoinstallcode/settings.py
+++ howtoinstallcode/settings.py
@@ -134,7 +134,12 @@ STATIC_URL = "staticfiles/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+STATICFILES_FINDERS = (
+  'django.contrib.staticfiles.finders.FileSystemFinder',
+  'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
